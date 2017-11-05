d612cea2b5a1802e7d0817afa0a4d30981018e56
Ярослав <yaroslawtkachenko@gmail.com>
2017-11-05 22:53:55 +0200

    Server

---
+++ HttpCalcServer/Calculate.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HttpCalcServer
+{
+    class Calculate
+    {
+        public static int Calc(string a, string b,string op)
+        {
+            int res = 0;
+
+            int num1 = Convert.ToInt32(a);
+            int num2 = Convert.ToInt32(b);
+
+            switch(op)
+            {
+                case "+": res = num1 + num2; break;
+                case "-": res = num1 - num2; break;
+                case "*": res = num1 * num2; break;
+                case "/": res = num1 / num2; break;
+            }
+            Console.WriteLine(res);
+            return res;
+        }
+    }
+}
---
+++ HttpCalcServer/HttpServer.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HttpCalcServer
+{
+    class HttpServer
+    {
+        HttpListener listener;
+
+        public HttpServer(string uri)
+        {
+            listener = new HttpListener();
+            listener.Prefixes.Add(uri);
+        }
+
+        public void Start()
+        {
+            listener.Start();
+            Console.WriteLine("Listening...");
+
+            while(true)
+            {
+                try
+                {
+                    HttpListenerContext context = listener.GetContext();
+                    Console.WriteLine("Join To Server");
+                    Receiver(context);
+                }
+                catch
+                {
+
+                }
+            }
+        }
+
+        public void Receiver(HttpListenerContext context)
+        {
+            string a = "";
+            string b = "";
+            string op = "";
+
+            if(context.Request.HttpMethod == "POST")
+            {
+                string[] param = new StreamReader(context.Request.InputStream).ReadToEnd().Split('=', '&');
+                a = param[1];
+                b = param[3];
+                op = param[5];
+            }
+            else
+            {
+                a = context.Request.QueryString["a"];
+                b = context.Request.QueryString["b"];
+                op = context.Request.QueryString["op"];
+            }
+
+            string data = Calculate.Calc(a, b, op).ToString();
+
+            context.Response.ContentType = "text/plain";
+            context.Response.AddHeader("Access-Control-Allow-Origin", "*");
+            context.Response.ContentLength64 = data.Length;
+            context.Response.OutputStream.Write(Encoding.ASCII.GetBytes(data), 0, data.Length);
+            //context.Response.OutputStream.Close();
+        }
+    }
+}
